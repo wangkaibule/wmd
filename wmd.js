21a9975fe01b2ad0b9266156c41802a1da697405
Anand Chitipothu <anandology@gmail.com>
2010-03-19 20:25:57 +0530

    refactor step 8: move UndoManager out

--- wmd.js
+++ wmd.js
@@ -1003,6 +1003,251 @@ var PreviewManager = function(wmd){ // {{{
     init();
 }; // }}}
 
+// Handles pushing and popping TextareaStates for undo/redo commands.
+// I should rename the stack variables to list.
+var UndoManager = function(textarea, pastePollInterval, callback){ // {{{
+
+    var undoObj = this;
+    var undoStack = []; // A stack of undo states
+    var stackPtr = 0; // The index of the current state
+    var mode = "none";
+    var lastState; // The last state
+    var poller;
+    var timer; // The setTimeout handle for cancelling the timer
+    var inputStateObj;
+    
+    // Set the mode for later logic steps.
+    var setMode = function(newMode, noSave){
+    
+        if (mode != newMode) {
+            mode = newMode;
+            if (!noSave) {
+                saveState();
+            }
+        }
+        
+        if (!browser.isIE || mode != "moving") {
+            timer = top.setTimeout(refreshState, 1);
+        }
+        else {
+            inputStateObj = null;
+        }
+    };
+    
+    var refreshState = function(){
+        inputStateObj = new TextareaState(textarea);
+        poller.tick();
+        timer = undefined;
+    };
+    
+    this.setCommandMode = function(){
+        mode = "command";
+        saveState();
+        timer = top.setTimeout(refreshState, 0);
+    };
+    
+    this.canUndo = function(){
+        return stackPtr > 1;
+    };
+    
+    this.canRedo = function(){
+        if (undoStack[stackPtr + 1]) {
+            return true;
+        }
+        return false;
+    };
+    
+    // Removes the last state and restores it.
+    this.undo = function(){
+    
+        if (undoObj.canUndo()) {
+            if (lastState) {
+                // What about setting state -1 to null or checking for undefined?
+                lastState.restore();
+                lastState = null;
+            }
+            else {
+                undoStack[stackPtr] = new TextareaState(textarea);
+                undoStack[--stackPtr].restore();
+                
+                if (callback) {
+                    callback();
+                }
+            }
+        }
+        
+        mode = "none";
+        textarea.focus();
+        refreshState();
+    };
+    
+    // Redo an action.
+    this.redo = function(){
+    
+        if (undoObj.canRedo()) {
+        
+            undoStack[++stackPtr].restore();
+            
+            if (callback) {
+                callback();
+            }
+        }
+        
+        mode = "none";
+        textarea.focus();
+        refreshState();
+    };
+    
+    // Push the input area state to the stack.
+    var saveState = function(){
+    
+        var currState = inputStateObj || new TextareaState(textarea);
+        
+        if (!currState) {
+            return false;
+        }
+        if (mode == "moving") {
+            if (!lastState) {
+                lastState = currState;
+            }
+            return;
+        }
+        if (lastState) {
+            if (undoStack[stackPtr - 1].text != lastState.text) {
+                undoStack[stackPtr++] = lastState;
+            }
+            lastState = null;
+        }
+        undoStack[stackPtr++] = currState;
+        undoStack[stackPtr + 1] = null;
+        if (callback) {
+            callback();
+        }
+    };
+    
+    var handleCtrlYZ = function(event){
+    
+        var handled = false;
+        
+        if (event.ctrlKey || event.metaKey) {
+        
+            // IE and Opera do not support charCode.
+            var keyCode = event.charCode || event.keyCode;
+            var keyCodeChar = String.fromCharCode(keyCode);
+            
+            switch (keyCodeChar) {
+            
+                case "y":
+                    undoObj.redo();
+                    handled = true;
+                    break;
+                    
+                case "z":
+                    if (!event.shiftKey) {
+                        undoObj.undo();
+                    }
+                    else {
+                        undoObj.redo();
+                    }
+                    handled = true;
+                    break;
+            }
+        }
+        
+        if (handled) {
+            if (event.preventDefault) {
+                event.preventDefault();
+            }
+            if (top.event) {
+                top.event.returnValue = false;
+            }
+            return;
+        }
+    };
+    
+    // Set the mode depending on what is going on in the input area.
+    var handleModeChange = function(event){
+    
+        if (!event.ctrlKey && !event.metaKey) {
+        
+            var keyCode = event.keyCode;
+            
+            if ((keyCode >= 33 && keyCode <= 40) || (keyCode >= 63232 && keyCode <= 63235)) {
+                // 33 - 40: page up/dn and arrow keys
+                // 63232 - 63235: page up/dn and arrow keys on safari
+                setMode("moving");
+            }
+            else if (keyCode == 8 || keyCode == 46 || keyCode == 127) {
+                // 8: backspace
+                // 46: delete
+                // 127: delete
+                setMode("deleting");
+            }
+            else if (keyCode == 13) {
+                // 13: Enter
+                setMode("newlines");
+            }
+            else if (keyCode == 27) {
+                // 27: escape
+                setMode("escape");
+            }
+            else if ((keyCode < 16 || keyCode > 20) && keyCode != 91) {
+                // 16-20 are shift, etc. 
+                // 91: left window key
+                // I think this might be a little messed up since there are
+                // a lot of nonprinting keys above 20.
+                setMode("typing");
+            }
+        }
+    };
+    
+    var setEventHandlers = function(){
+    
+        util.addEvent(textarea, "keypress", function(event){
+            // keyCode 89: y
+            // keyCode 90: z
+            if ((event.ctrlKey || event.metaKey) && (event.keyCode == 89 || event.keyCode == 90)) {
+                event.preventDefault();
+            }
+        });
+        
+        var handlePaste = function(){
+            if (browser.isIE || (inputStateObj && inputStateObj.text != textarea.value)) {
+                if (timer == undefined) {
+                    mode = "paste";
+                    saveState();
+                    refreshState();
+                }
+            }
+        };
+        
+        poller = new InputPoller(textarea, handlePaste, pastePollInterval);
+        
+        util.addEvent(textarea, "keydown", handleCtrlYZ);
+        util.addEvent(textarea, "keydown", handleModeChange);
+        
+        util.addEvent(textarea, "mousedown", function(){
+            setMode("moving");
+        });
+        textarea.onpaste = handlePaste;
+        textarea.ondrop = handlePaste;
+    };
+    
+    var init = function(){
+        setEventHandlers();
+        refreshState();
+        saveState();
+    };
+    
+    this.destroy = function(){
+        if (poller) {
+            poller.destroy();
+        }
+    };
+    
+    init();
+}; //}}}
+
 WMDEditor.util = util;
 WMDEditor.position = position;
 
@@ -1055,251 +1300,6 @@ var wmdBase = function(wmd, wmd_options){ // {{{
     // normally since the focus never leaves the textarea.
     wmd.ieCachedRange = null;        // cached textarea selection
     wmd.ieRetardedClick = false;    // flag
-    
-    // Handles pushing and popping TextareaStates for undo/redo commands.
-    // I should rename the stack variables to list.
-    wmd.undoManager = function(callback){ // {{{
-    
-        var undoObj = this;
-        var undoStack = []; // A stack of undo states
-        var stackPtr = 0; // The index of the current state
-        var mode = "none";
-        var lastState; // The last state
-        var poller;
-        var timer; // The setTimeout handle for cancelling the timer
-        var inputStateObj;
-        
-        // Set the mode for later logic steps.
-        var setMode = function(newMode, noSave){
-        
-            if (mode != newMode) {
-                mode = newMode;
-                if (!noSave) {
-                    saveState();
-                }
-            }
-            
-            if (!browser.isIE || mode != "moving") {
-                timer = top.setTimeout(refreshState, 1);
-            }
-            else {
-                inputStateObj = null;
-            }
-        };
-        
-        var refreshState = function(){
-            inputStateObj = new TextareaState(wmd.panels.input);
-            poller.tick();
-            timer = undefined;
-        };
-        
-        this.setCommandMode = function(){
-            mode = "command";
-            saveState();
-            timer = top.setTimeout(refreshState, 0);
-        };
-        
-        this.canUndo = function(){
-            return stackPtr > 1;
-        };
-        
-        this.canRedo = function(){
-            if (undoStack[stackPtr + 1]) {
-                return true;
-            }
-            return false;
-        };
-        
-        // Removes the last state and restores it.
-        this.undo = function(){
-        
-            if (undoObj.canUndo()) {
-                if (lastState) {
-                    // What about setting state -1 to null or checking for undefined?
-                    lastState.restore();
-                    lastState = null;
-                }
-                else {
-                    undoStack[stackPtr] = new TextareaState(wmd.panels.input);
-                    undoStack[--stackPtr].restore();
-                    
-                    if (callback) {
-                        callback();
-                    }
-                }
-            }
-            
-            mode = "none";
-            wmd.panels.input.focus();
-            refreshState();
-        };
-        
-        // Redo an action.
-        this.redo = function(){
-        
-            if (undoObj.canRedo()) {
-            
-                undoStack[++stackPtr].restore();
-                
-                if (callback) {
-                    callback();
-                }
-            }
-            
-            mode = "none";
-            wmd.panels.input.focus();
-            refreshState();
-        };
-        
-        // Push the input area state to the stack.
-        var saveState = function(){
-        
-            var currState = inputStateObj || new TextareaState(wmd.panels.input);
-            
-            if (!currState) {
-                return false;
-            }
-            if (mode == "moving") {
-                if (!lastState) {
-                    lastState = currState;
-                }
-                return;
-            }
-            if (lastState) {
-                if (undoStack[stackPtr - 1].text != lastState.text) {
-                    undoStack[stackPtr++] = lastState;
-                }
-                lastState = null;
-            }
-            undoStack[stackPtr++] = currState;
-            undoStack[stackPtr + 1] = null;
-            if (callback) {
-                callback();
-            }
-        };
-        
-        var handleCtrlYZ = function(event){
-        
-            var handled = false;
-            
-            if (event.ctrlKey || event.metaKey) {
-            
-                // IE and Opera do not support charCode.
-                var keyCode = event.charCode || event.keyCode;
-                var keyCodeChar = String.fromCharCode(keyCode);
-                
-                switch (keyCodeChar) {
-                
-                    case "y":
-                        undoObj.redo();
-                        handled = true;
-                        break;
-                        
-                    case "z":
-                        if (!event.shiftKey) {
-                            undoObj.undo();
-                        }
-                        else {
-                            undoObj.redo();
-                        }
-                        handled = true;
-                        break;
-                }
-            }
-            
-            if (handled) {
-                if (event.preventDefault) {
-                    event.preventDefault();
-                }
-                if (top.event) {
-                    top.event.returnValue = false;
-                }
-                return;
-            }
-        };
-        
-        // Set the mode depending on what is going on in the input area.
-        var handleModeChange = function(event){
-        
-            if (!event.ctrlKey && !event.metaKey) {
-            
-                var keyCode = event.keyCode;
-                
-                if ((keyCode >= 33 && keyCode <= 40) || (keyCode >= 63232 && keyCode <= 63235)) {
-                    // 33 - 40: page up/dn and arrow keys
-                    // 63232 - 63235: page up/dn and arrow keys on safari
-                    setMode("moving");
-                }
-                else if (keyCode == 8 || keyCode == 46 || keyCode == 127) {
-                    // 8: backspace
-                    // 46: delete
-                    // 127: delete
-                    setMode("deleting");
-                }
-                else if (keyCode == 13) {
-                    // 13: Enter
-                    setMode("newlines");
-                }
-                else if (keyCode == 27) {
-                    // 27: escape
-                    setMode("escape");
-                }
-                else if ((keyCode < 16 || keyCode > 20) && keyCode != 91) {
-                    // 16-20 are shift, etc. 
-                    // 91: left window key
-                    // I think this might be a little messed up since there are
-                    // a lot of nonprinting keys above 20.
-                    setMode("typing");
-                }
-            }
-        };
-        
-        var setEventHandlers = function(){
-        
-            util.addEvent(wmd.panels.input, "keypress", function(event){
-                // keyCode 89: y
-                // keyCode 90: z
-                if ((event.ctrlKey || event.metaKey) && (event.keyCode == 89 || event.keyCode == 90)) {
-                    event.preventDefault();
-                }
-            });
-            
-            var handlePaste = function(){
-                if (browser.isIE || (inputStateObj && inputStateObj.text != wmd.panels.input.value)) {
-                    if (timer == undefined) {
-                        mode = "paste";
-                        saveState();
-                        refreshState();
-                    }
-                }
-            };
-            
-            poller = new InputPoller(wmd.panels.input, handlePaste, wmd_options.pastePollInterval);
-            
-            util.addEvent(wmd.panels.input, "keydown", handleCtrlYZ);
-            util.addEvent(wmd.panels.input, "keydown", handleModeChange);
-            
-            util.addEvent(wmd.panels.input, "mousedown", function(){
-                setMode("moving");
-            });
-            wmd.panels.input.onpaste = handlePaste;
-            wmd.panels.input.ondrop = handlePaste;
-        };
-        
-        var init = function(){
-            setEventHandlers();
-            refreshState();
-            saveState();
-        };
-        
-        this.destroy = function(){
-            if (poller) {
-                poller.destroy();
-            }
-        };
-        
-        init();
-    }; //}}}
     
     // I think my understanding of how the buttons and callbacks are stored in the array is incomplete.
     wmd.editor = function(previewRefreshCallback){ // {{{
@@ -1546,7 +1546,7 @@ var wmdBase = function(wmd, wmd_options){ // {{{
             }
             
             if (!wmd.nativeUndo) {
-                undoMgr = new wmd.undoManager(function(){
+                undoMgr = new UndoManager(wmd.panels.input, wmd.options.pastePollInterval, function(){
                     previewRefreshCallback();
                     setUndoRedoButtonStates();
                 });
